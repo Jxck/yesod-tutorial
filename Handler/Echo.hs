91a2c26e62fb2a133f1bc818246cf43115eda08b
Jxck <block.rxckin.beats@gmail.com>
2012-05-02 23:10:24 +0900

    String to Data.Text

--- Handler/Echo.hs
+++ Handler/Echo.hs
@@ -2,7 +2,7 @@ module Handler.Echo where
 
 import Import
 
-getEchoR :: String -> Handler RepHtml
+getEchoR :: Text -> Handler RepHtml
 getEchoR theText = do
     defaultLayout $ do
         [whamlet|<h1>#{theText}|]
